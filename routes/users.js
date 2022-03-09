336f6eb97e3ea6ff8828b26146a6db6cb16692f9
LPP <2175801@cegep-heritage.qc.ca>
2022-03-09 13:52:33 -0500

    Very basic but works

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:whatever', function(req, res, next) {
+
+   res.render('users', {'whatever':req.params.whatever});
+
 });
 
 module.exports = router;
