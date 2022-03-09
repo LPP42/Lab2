630a2683267afc59afed383a3993b9a68b8a68b9
LPP <2175801@cegep-heritage.qc.ca>
2022-03-09 14:08:32 -0500

    Project Banana aborted

---
+++ routes/bananas.js
@@ -0,0 +1,13 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/:whatever/:bananas', function(req, res, next) {
+
+//    res.render('users', {'bananas':req.params.bananas});
+
+// });
+
+// module.exports = router;
+
+// That not how this works
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //
-  res.render('index', { title: 'Lab 0.2' });
+  res.render('index', { title: 'Lab 2' });
 });
 
 module.exports = router;
